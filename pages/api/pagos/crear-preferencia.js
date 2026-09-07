6a899c18614dcef042b79e6eddb5cf34df83ff65
Jeronimo <jejoacu@gmail.com>
2026-09-07 11:36:43 -0300

    Solucionamos error de mp

--- pages/api/pagos/crear-preferencia.js
+++ pages/api/pagos/crear-preferencia.js
@@ -50,7 +50,6 @@ export default async function handler(req, res) {
       monto,
       baseUrl,
     })
-
     return res.status(200).json({ url: initPoint })
   } catch (err) {
     console.error('Error al crear preferencia:', err)
